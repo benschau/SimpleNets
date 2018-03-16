edbd6f90ec4df854e3a5add0b8affe52ac179e3c
Benson Chau <bchau3440@gmail.com>
2018-03-16 16:23:31 -0700

    init commit

---
+++ mnist.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as np
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+class MNIST:
+    def __init__(self, data, labels, mode):
+        
+
+if __name__ == "__main__":
+	
