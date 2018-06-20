be43c9c74eebfa7acf16d134109eb8e6071ea3fa
Benson Chau <bchau3440@gmail.com>
2018-06-19 23:53:10 -0400

    added self references, loaded europarl module

--- rnn_encoderdecoder/input_data.py
+++ rnn_encoderdecoder/input_data.py
@@ -7,15 +7,15 @@ dataset = "data/training/"
 class europarl:
     
     def __init__(self):
-        self.english = process_file('europarl-v7.fr-en.en')
-        self.french = process_file('europarl-v7.fr-en.fr')
+        self.english = self.process_file('europarl-v7.fr-en.en')
+        self.french = self.process_file('europarl-v7.fr-en.fr')
 
     def process_file(self, filename):
-        sentences = load_file(filename)
+        sentences = self.load_file(filename)
         lengths = [len(s.split()) for s in sentences]
         max_len = max(lengths)
         
-        return (clean_data(sentences), max_len)
+        return (self.clean_data(sentences), max_len)
 
     def load_file(self, filename):
         file = open(dataset + filename, mode='rt', encoding='utf-8')
--- rnn_encoderdecoder/nn_model.py
+++ rnn_encoderdecoder/nn_model.py
@@ -7,8 +7,12 @@ RNN Encoder-Decoder for Statistical Machine Translation (Cho et. al).
 Author: Benson Chau
 """
 
+import importlib
 import tensorflow as tf
 import numpy as np
+
+europarl_module = importlib.import_module('input_data')
+europarl = getattr(europarl_module, 'europarl')
 
 # Training Parameters
 learning_rate = 0.001
@@ -17,7 +21,8 @@ batch_size = 128
 display_step = 200
 
 # Network Parameters
-num_input = 
+max_num_input = 50
 timesteps = 28 # timesteps 
 num_hidden = 1000 # hidden layer num of features
 
+data = europarl()
